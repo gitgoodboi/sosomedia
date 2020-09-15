b10de0cd910a2bebb2ffadae032964a11ccdd843
haude419 <70281188+haude419@users.noreply.github.com>
2020-09-15 14:27:25 +0545

    Timeline Second Draft

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -9,20 +9,26 @@ import {MdAddCircleOutline} from "react-icons/md";
 function Navbar(){
 
     return(
+
+// ===================================================MAIN NAVBAR SECTION STARTS==================================
         <div className='navbar' >
         <div style={{height: '64px', backgroundColor: "white", opacity: "1"}} className='fixed z-50 bottom-0 w-screen flex justify-center border-solid border-t-2
         lg:top-0 h-16 border-solid border-b-2 items-center'>
-        {/* <nav class='w-screen' > */}
 
-
+{/* ================================================NAVBAR LARGE ITEMS============================================ */}
             <h1 className='hello hidden lg:block py-5 px-10 font-micolestherregular ' >SOSOMEDIA</h1>
+
             <div className='hidden lg:flex items-center left-1/2 py-0 px-10 top-0 '>
                 <AiOutlineSearch size={22} className='searchpad' />
                 <input type={Text}  placeholder="SEARCH" className='outline-none px-2 '></input>
             </div>
+{/* =================================================END NAVBAR LARGE ITEMS==================================== */}
+            
 
+{/* ====================================================NAVBAR ICONS MADE USING LIST===============================             */}
             <ul class='w-screen flex justify-between px-1 py-1
             lg:justify-end w-auto'>
+
                 <Link to='/sosomedia' >
                 <li className='usedicon' className='lg:px-8 hover:bg-gray-200 rounded-full '>
                     <AiOutlineHome size={32} />
@@ -32,25 +38,30 @@ function Navbar(){
                 <li className='hidden lg:block px-8 hover:bg-gray-200 rounded-full'>
                     <AiOutlineCompass size={32} />
                 </li>
+
                 <li className='lg:hidden hover:bg-gray-200 rounded-full'>
                     <AiOutlineSearch size={32} />
                 </li>
+
                 <li className='lg:hidden hover:bg-gray-200 rounded-full'>
                     <MdAddCircleOutline size={32} />
                 </li>
+
                 <li className='lg:px-8 hover:bg-gray-200 rounded-full'>
                     <AiOutlineHeart size={32} />
                 </li>
+
                 <Link to='/profile' >
                 <li className='lg:px-8 hover:bg-gray-200 rounded-full' >
                     <AiOutlineUser size={32} />
                 </li>
                 </Link>
-                
+
             </ul>
-        {/* </nav> */}
+{/* ==========================================NAVBAR ICONS END================================================= */}
         </div>
         </div>
+// ===================================================NAVBAR CODE ENDS============================================
     )
 }
 
--- src/Homepage/index.js
+++ src/Homepage/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import './styles.css'
 import Storyline from './../Components/Storyline';
 import DjAnkit from "../../src/img/Djankit.jpg";
-import partyman from '../img/partymanankit.jpg'
+import partyman from '../img/partymanankit.jpg';
+import artistman from '../img/artistmanankit.jpg';
+import porchman from '../img/porchmanankit.jpg';
+import danceman from '../img/dancemanankit.jpg'
 import { AiOutlineUser, AiOutlineHeart, AiOutlineComment } from "react-icons/ai"
 import { HiDotsVertical, HiChatAlt } from "react-icons/hi"
 import { BsBookmark } from "react-icons/bs"
@@ -13,47 +16,85 @@ import { Link } from 'react-router-dom'
 function Homepage(){
     return(
         <section>
+
             <Storyline></Storyline>
 
-        <section style={{ height: '5000px', paddingTop: '64px'}} className='justify-center flex w-full
+{/* ========================================================POSTS BEGIN=========================================== */}
+        <section style={{ height: '5000px', paddingTop: '64px'}} className='justify-center flex-row w-full
         lg:w-500px' >
 
-            <div style={{height: '50px'}} className='block flex justify-center' > 
 
-                <Link to='/profile' >
-                <div className='flex absolute left-0 ' >
-                <img
+{/* ========================================================FIRST POST====================================================== */}
+
+<div className='lg:w-1/2' >
+{/* ======================================================POSTER 1 INFO================================================ */}
+            <div style={{height: '50px'}} className='block flex justify-center' > 
+                
+            <div className='flex justify-between absolute left-0 lg:w-2/5' >
+
+                <div className='flex justify-between' >
+                    <Link to='/profile' >
+                    <div className='flex' >
+                    <img
                     style={{marginLeft: '4px'}}
                     className="w-10 h-10 rounded-full"
                     src={DjAnkit}
                     alt={"RamroDjMaybe"}
                     ></img>
                     <p style={{marginLeft: '4px', fontWeight: 'bold', paddingTop: '5px' }} >DJAnkit</p>
+                    </div>
+                    </Link>
                 </div>
-                </Link>
-               
-                <div className='absolute right-0 pt-3' >
-                    <HiDotsVertical />
+
+                <div className='hidden lg:block absolute right-0' >
+                    <HiDotsVertical size={25} />
                 </div>
             </div>
+                
+                <div className='absolute right-0 pt-3 lg:hidden' >
+                    <HiDotsVertical size={25} />
+                </div>
 
-            <div className='pt-12 w-full md:w-5/6' >
+            </div>
+
+{/* =======================================================POSTER 1 INFO ENDS========================================== */}
+
+{/* =======================================================MAIN POST 1 BEGINS========================================= */}
+            <div className='w-full md:w-5/6' >
+                <div className="md:ml-40 lg:ml-6" >
                 <img src={partyman} alt='partymanankit' ></img>
+                </div>
+                
+
+{/* ================================================USER'S LIKES & COMMENTS BAR 1 SECTION=============================== */}
+
                 <div style={{paddingTop: '40px'}} className='flex justify-center ' >
-                    <div className='absolute flex left-0' >
+
+                    <div className='absolute flex left-0 lg:w-5/12' >
                     <AiOutlineHeart size={30} className='mx-1 md:mx-2 lg:mx-4' />
                     <AiOutlineComment size={30} className='md:mx-2 lg:mx-4' />
+
+                    <div className='hidden lg:block absolute right-0' >
+                        <BsBookmark size={30} />
                     </div>
+                    </div>
+
                     <div className='absolute right-0' >
-                    <BsBookmark style={{float: 'right'}} className='md:mx-2 lg:mx-4' size={30}/>
+                    <BsBookmark style={{float: 'right'}} className='md:mx-2 lg:hidden' size={30}/>
                     </div>
                 </div>
-                <div className='pt-10'>
 
+{/* ===========================================USER'S LIKES & COMMENTS BAR 1 ENDS=============================== */}
+           
+{/* ==============================================LIKES AND COMMENTS 1============================================== */}
+            <div className='pt-10'>
+
+                {/* LIKES COUNT */}
                 <div>
                     <p className='font-bold' >747 Likes</p>
                 </div>
 
+{/* ==============================================MAIN COMMENT SECTION 1============================================ */}
                     
                 <div>
                     <input type="checkbox" className="read-more-state" id='commentmore' ></input>
@@ -81,15 +122,310 @@ function Homepage(){
                     </ul>
                     <label className='read-more-trigger' for='commentmore'  ></label>
                 </div>
-
+{/* ==================================================MAIN COMMENT SECTION 1 ENDS=============================== */}
+               
                 </div>
+{/* ===============================================LIKES AND COMMENT SECTION 1 ENDS=============================== */}
                 
-
             </div>
 
-        </section>
+{/* ====================================================MAIN POST 1 ENDS======================================================= */}
+
+</div>
+{/* =======================================================FIRST POST ENDS========================================================= */}
+
+
+
+
+
+<div className='pt-12 lg:w-1/2' >
+
+        <div className='flex' >
+
+        <div className='flex justify-between lg:w-5/6 ' >
+        <Link to='/profile'>
+        <div className='flex' >
+            <img
+                style={{marginLeft: '4px'}} 
+                className='w-10 h-10 rounded-full'
+                src={DjAnkit} 
+                alt='djankit' ></img>
+                <p style={{marginLeft: '4px', fontWeight: 'bold', paddingTop: '5px' }} >DJ ANKIT</p>
+        </div>
+        </Link>
+        <div className='hidden lg:block' >
+        <HiDotsVertical size={25} /> 
+        </div>
+        </div>
+        
+
+
+        <div className='absolute right-0 lg:hidden ' >
+            <HiDotsVertical size={25} />
+        </div>
+
+        </div>
+
+        <div>
+            <div className='w-full md:w-5/6' >
+                <div className="md:ml-40 lg:ml-6" >
+                 <img src={artistman} alt='artistman' ></img>
+                </div>
+            </div>
+        </div>
+        <div className='flex' >
+
+
+        <div className='flex' >
+            <AiOutlineHeart size={30} className='mx-1 md:mx-2 lg:mx-4' />
+            <AiOutlineComment size={30} className='md:mx-2 lg:mx-4' />
+
+            <div className='hidden lg:block ml-64' >
+            <BsBookmark size={30} />
+            </div>
+            
+        </div>
+        <div className='absolute right-0 lg:hidden'>
+        <BsBookmark size={30} />
+        </div>
+
+        </div>    
+
+        <div style={{paddingLeft: '5px'}} >
+            <p style={{fontWeight: 'bold'}} >2,342 Likes</p>
+        </div>
+
+        <div style={{paddingTop: '5px' , paddingLeft: '5px' }} >
+            <input type='checkbox' id='commentmoretwo' className='read-more-state-two' ></input>
+
+            <ul className='read-more-wrap-two' >
+                <li>
+                <div className='flex' >
+                    <p style={{fontWeight: 'bold'}} >pemba.10</p><p>Can you count the stars</p>
+                </div>  
+                </li>
+                <li>
+                <div className='read-more-target-two flex' >
+                    <p style={{fontWeight: 'bold'}} >ninu_nimi</p><p>pimple chai extra ayecha</p>
+                </div>  </li>
+                <li>
+                <div className='read-more-target-two flex' >
+                    <p style={{fontWeight: 'bold'}} >notalawyer</p><p>i can hear this image</p>
+                </div>  </li>
+                <li>
+                <div className='read-more-target-two flex' >
+                    <p style={{fontWeight: 'bold'}} >daf.an</p><p>Creative</p>
+                </div>  </li>
+                <li>
+                <div className='read-more-target-two flex' >
+                    <p style={{fontWeight: 'bold'}} >zeantoniojr</p><p>You have to wear Calvin Klein clothes</p>
+                </div>  </li>
+            </ul>
+
+            <label className='read-more-trigger-two' for='commentmoretwo' ></label>
+
+        </div>
+            
+
+</div>
+
+
+
+<div className='pt-12 lg:w-1/2' >
+
+        <div className='flex' >
+
+        <div className='flex justify-between lg:w-5/6 ' >
+        <Link to='/profile'>
+        <div className='flex' >
+            <img
+                style={{marginLeft: '4px'}} 
+                className='w-10 h-10 rounded-full'
+                src={DjAnkit} 
+                alt='djankit' ></img>
+                <p style={{marginLeft: '4px', fontWeight: 'bold', paddingTop: '5px' }} >DJ ANKIT</p>
+        </div>
+        </Link>
+        <div className='hidden lg:block' >
+        <HiDotsVertical size={25} /> 
+        </div>
+        </div>
+        
+
+
+        <div className='absolute right-0 lg:hidden ' >
+            <HiDotsVertical size={25} />
+        </div>
+
+        </div>
+
+        <div>
+            <div className='w-full md:w-5/6' >
+                <div className="md:ml-40 lg:ml-6" >
+                 <img src={porchman} alt='artistman' ></img>
+                </div>
+            </div>
+        </div>
+        <div className='flex' >
+
+
+        <div className='flex' >
+            <AiOutlineHeart size={30} className='mx-1 md:mx-2 lg:mx-4' />
+            <AiOutlineComment size={30} className='md:mx-2 lg:mx-4' />
+
+            <div className='hidden lg:block ml-64' >
+            <BsBookmark size={30} />
+            </div>
+            
+        </div>
+        <div className='absolute right-0 lg:hidden'>
+        <BsBookmark size={30} />
+        </div>
+
+        </div>    
+
+        <div style={{paddingLeft: '5px'}} >
+            <p style={{fontWeight: 'bold'}} >2,342 Likes</p>
+        </div>
+
+        <div style={{paddingTop: '5px' , paddingLeft: '5px' }} >
+            <input type='checkbox' id='commentmorethree' className='read-more-state-three' ></input>
+
+            <ul className='read-more-wrap-three' >
+                <li>
+                <div className='flex' >
+                    <p style={{fontWeight: 'bold'}} >pemba.10</p><p>Can you count the stars</p>
+                </div>  
+                </li>
+                <li>
+                <div className='read-more-target-three flex' >
+                    <p style={{fontWeight: 'bold'}} >ninu_nimi</p><p>pimple chai extra ayecha</p>
+                </div>  </li>
+                <li>
+                <div className='read-more-target-three flex' >
+                    <p style={{fontWeight: 'bold'}} >notalawyer</p><p>i can hear this image</p>
+                </div>  </li>
+                <li>
+                <div className='read-more-target-three flex' >
+                    <p style={{fontWeight: 'bold'}} >daf.an</p><p>Creative</p>
+                </div>  </li>
+                <li>
+                <div className='read-more-target-three flex' >
+                    <p style={{fontWeight: 'bold'}} >zeantoniojr</p><p>You have to wear Calvin Klein clothes</p>
+                </div>  </li>
+            </ul>
+
+            <label className='read-more-trigger-three' for='commentmorethree' ></label>
+
+        </div>
+            
+
+</div>
+
+
+
+
+
+<div className='pt-12 lg:w-1/2' >
+
+        <div className='flex' >
+
+        <div className='flex justify-between lg:w-5/6 ' >
+        <Link to='/profile'>
+        <div className='flex' >
+            <img
+                style={{marginLeft: '4px'}} 
+                className='w-10 h-10 rounded-full'
+                src={DjAnkit} 
+                alt='djankit' ></img>
+                <p style={{marginLeft: '4px', fontWeight: 'bold', paddingTop: '5px' }} >DJ ANKIT</p>
+        </div>
+        </Link>
+        <div className='hidden lg:block' >
+        <HiDotsVertical size={25} /> 
+        </div>
+        </div>
+        
+
+
+        <div className='absolute right-0 lg:hidden ' >
+            <HiDotsVertical size={25} />
+        </div>
+
+        </div>
+
+        <div>
+            <div className='w-full md:w-5/6' >
+                <div className="md:ml-40 lg:ml-6" >
+                 <img src={danceman} alt='artistman' ></img>
+                </div>
+            </div>
+        </div>
+        <div className='flex' >
+
+
+        <div className='flex' >
+            <AiOutlineHeart size={30} className='mx-1 md:mx-2 lg:mx-4' />
+            <AiOutlineComment size={30} className='md:mx-2 lg:mx-4' />
+
+            <div className='hidden lg:block ml-64' >
+            <BsBookmark size={30} />
+            </div>
+            
+        </div>
+        <div className='absolute right-0 lg:hidden'>
+        <BsBookmark size={30} />
+        </div>
+
+        </div>    
+
+        <div style={{paddingLeft: '5px'}} >
+            <p style={{fontWeight: 'bold'}} >2,342 Likes</p>
+        </div>
+
+        <div style={{paddingTop: '5px' , paddingLeft: '5px' }} >
+            <input type='checkbox' id='commentmorefour' className='read-more-state-four' ></input>
+
+            <ul className='read-more-wrap-four' >
+                <li>
+                <div className='flex' >
+                    <p style={{fontWeight: 'bold'}} >pemba.10</p><p>Can you count the stars</p>
+                </div>  
+                </li>
+                <li>
+                <div className='read-more-target-four flex' >
+                    <p style={{fontWeight: 'bold'}} >ninu_nimi</p><p>pimple chai extra ayecha</p>
+                </div>  </li>
+                <li>
+                <div className='read-more-target-four flex' >
+                    <p style={{fontWeight: 'bold'}} >notalawyer</p><p>i can hear this image</p>
+                </div>  </li>
+                <li>
+                <div className='read-more-target-four flex' >
+                    <p style={{fontWeight: 'bold'}} >daf.an</p><p>Creative</p>
+                </div>  </li>
+                <li>
+                <div className='read-more-target-four flex' >
+                    <p style={{fontWeight: 'bold'}} >zeantoniojr</p><p>You have to wear Calvin Klein clothes</p>
+                </div>  </li>
+            </ul>
+
+            <label className='read-more-trigger-four' for='commentmorefour' ></label>
+
+        </div>
+            
+
+</div>
 
         </section>
+
+{/* ========================================================POSTS ENDS========================================================== */}
+
+
+
+
+        </section>
+
     )
 }
 
